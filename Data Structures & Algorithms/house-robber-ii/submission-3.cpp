383ae7c596f838f088d479f6b5383fc9ff6a62b1
Tarun1-6 <tarunpatnaik16@gmail.com>
2026-08-19 03:18:42 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int robHelper(vector<int>& nums, int start, int end) {
+        int first = nums[start];
+        int sec = max(nums[start], nums[start + 1]);
+        if (end-start == 2) return sec;
+        int curr = 0;
+        for (int i = start + 2; i < end; ++i) {
+            curr = max(sec, first + nums[i]);
+            first = sec;
+            sec = curr;
+        }
+        return curr;
+    }
+
+   public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+        return max(robHelper(nums, 0, n - 1), robHelper(nums, 1, n));
+    }
+};
